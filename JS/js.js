df43a700cf51eb7b6beddda97bc6e3c56daa8e3e
Dading F Sukma <43590820+dadingfs@users.noreply.github.com>
2021-02-27 12:58:52 +0700

    Add files via upload

---
+++ JS/js.js
@@ -0,0 +1,8 @@
+function contact() {
+    var x = document.getElementById("social");
+    if (x.style.visibility === "visible") {
+      x.style.visibility = "hidden";
+    } else {
+      x.style.visibility = "visible";
+    }
+  }
